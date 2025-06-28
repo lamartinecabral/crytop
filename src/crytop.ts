433ff47ae31075d854ce728671702641be6b534c
lamartinecabral <lamartine.cb@gmail.com>
2025-06-28 11:25:04 -0300

    improve code

--- src/crytop.ts
+++ src/crytop.ts
@@ -1,87 +1,84 @@
-export const encrypt = async (message: string, password: string) => {
-  const key = await importKey(password);
-  const encoded = new TextEncoder().encode(message);
-  const ciphertext = await crypto.subtle.encrypt(
-    {
-      name: "AES-CBC",
-      iv: new Uint8Array(16),
-    },
-    key,
-    encoded
-  );
-  const bytes = new Uint8Array(ciphertext);
-  const chars = [...bytes].map((byte) => String.fromCodePoint(byte));
-  return btoa(chars.join(""));
-};
+class Crytop {
+  generateKey;
+  encrypt;
+  decrypt;
 
-export const decrypt = async (encryptedMessage: string, password: string) => {
-  const key = await importKey(password);
-  const ciphertext = new Uint8Array(
-    atob(encryptedMessage)
-      .split("")
-      .map((a) => Number(a.codePointAt(0)))
-  ).buffer;
-  const decrypted = await crypto.subtle.decrypt(
-    {
-      name: "AES-CBC",
-      iv: new Uint8Array(16),
-    },
-    key,
-    ciphertext
-  );
-  return new TextDecoder().decode(decrypted);
-};
+  constructor() {
+    const constants = (() => {
+      const algorithm = "AES";
+      const mode = "CBC";
+      const length = 256;
+      const name = `${algorithm}-${mode}`;
 
+      return {
+        name,
+        keyAlgorithm: { name, length },
+        alg: `${algorithm[0]}${length}${mode}`,
+        ext: true,
+        key_ops: ["encrypt", "decrypt"] satisfies KeyUsage[],
+      };
+    })();
+
+    this.generateKey = (password: string) => {
+      if (typeof password !== "string") {
+        throw new TypeError("password must be a string");
+      }
+
+      const { alg, ext, key_ops, keyAlgorithm } = constants;
+
+      return crypto.subtle.importKey(
+        "jwk",
+        { alg, ext, k: btoa(password.padEnd(32, " ")), key_ops, kty: "oct" },
+        keyAlgorithm,
+        ext,
+        key_ops
+      );
+    };
+
+    this.encrypt = async (message: string, password: string) => {
+      const { name } = constants;
+      const passwordKey = await this.generateKey(password);
+
+      const messageEncoded = new TextEncoder().encode(message);
+
+      const ciphertext = await crypto.subtle.encrypt(
+        { name, iv: new Uint8Array(16) },
+        passwordKey,
+        messageEncoded
+      );
+
+      const bytes = new Uint8Array(ciphertext);
+      const chars = [...bytes].map((byte) => String.fromCodePoint(byte));
+      return btoa(chars.join(""));
+    };
+
+    this.decrypt = async (encryptedMessage: string, password: string) => {
+      const { name } = constants;
+      const passwordKey = await this.generateKey(password);
+
+      const chars = atob(encryptedMessage).split("");
+      const bytes = new Uint8Array(chars.map((a) => Number(a.codePointAt(0))));
+      const cyphertext = bytes.buffer;
+
+      const messageEncoded = await crypto.subtle.decrypt(
+        { name, iv: new Uint8Array(16) },
+        passwordKey,
+        cyphertext
+      );
+
+      return new TextDecoder().decode(messageEncoded);
+    };
+  }
+}
+
+const crytop = new Crytop();
+
+export const encrypt: typeof crytop.encrypt = (...args) => {
+  return crytop.encrypt(...args);
+};
+export const decrypt: typeof crytop.decrypt = (...args) => {
+  return crytop.decrypt(...args);
+};
 export const version: string = "";
 
-const Crytop = {
-  encrypt,
-  decrypt,
-  version,
-};
-
-export default Crytop;
-
-const importKey = async (str: string) => {
-  if (typeof str !== "string" || str.length > 32)
-    throw new TypeError("invalid key");
-  const key = toBase64(padEnd(str, 32, " "));
-  return crypto.subtle.importKey(...keyArgs(key));
-};
-
-const toBase64 = (str: string) => {
-  return btoa(str).replace(/[+/=]/g, (c) => {
-    return (
-      {
-        "+": "-",
-        "/": "_",
-        "=": "",
-      }[c] ?? c
-    );
-  });
-};
-
-const keyArgs = (k: string) =>
-  [
-    "jwk",
-    {
-      alg: "A256CBC",
-      ext: true,
-      k,
-      key_ops: ["encrypt", "decrypt"],
-      kty: "oct",
-    } satisfies JsonWebKey,
-    { name: "AES-CBC" },
-    true,
-    ["encrypt", "decrypt"],
-  ] as const;
-
-const padEnd = (str: string, maxLength: number, fillString: string) => {
-  return (
-    str +
-    Array(maxLength)
-      .fill(fillString[0])
-      .join("")
-      .slice(0, Math.max(0, maxLength - str.length))
-  );
-};
+export default { encrypt, decrypt, version };
